4dbd5ad44e7eadd66ce8952707a65c1175f21f6c
WilliamTrung <52268132+WilliamTrung@users.noreply.github.com>
2021-10-28 14:53:46 +0700

    Init CRUD --CategoryDAO, ProductDAO

--- DigitalDistributionStore/DataAccess/DataAccess/CategoryDAO.cs
+++ DigitalDistributionStore/DataAccess/DataAccess/CategoryDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessObject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,88 @@ namespace DataAccess
 {
     public class CategoryDAO
     {
+        public static List<Category> GetCategories()
+        {
+            var list = new List<Category>();
+            try
+            {
+                using (var context = new DBContext())
+                {
+                    list = context.Categories.ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return list;
+        }
+        public static void Insert(Category category)
+        {
+            try
+            {
+                using (var context = new DBContext())
+                {
+                    var check = context.Categories.SingleOrDefault(c => c.CategoryName == category.CategoryName);
+                    if (check == null)
+                        {
+                        context.Categories.Add(category);
+                        context.SaveChanges();
+                    }
+                    else
+                    {
+                        throw new Exception("This category has already existed!");
+                    }
+                }                   
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+            
+        
+        public static void Update(Category category)
+        {
+            try
+            {
+                using (var context = new DBContext())
+                {
+                    var check = context.Categories.SingleOrDefault(c => c.CategoryName == category.CategoryName);
+                    if (check == null)
+                    {
+                        context.Entry<Category>(category).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                        context.SaveChanges();
+                    }
+                    else
+                    {
+                        throw new Exception("This category has already existed!");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+        public static void Delete(Category category)
+        {
+            try
+            {
+                using (var context = new DBContext())
+                {
+                    var check = context.Categories.SingleOrDefault(c => c.CategoryID == category.CategoryID);
+                    if (check != null)
+                    {
+                        context.Categories.Remove(check);
+                        context.SaveChanges();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
     }
 }
--- DigitalDistributionStore/DataAccess/DataAccess/ProductDAO.cs
+++ DigitalDistributionStore/DataAccess/DataAccess/ProductDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessObject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,90 @@ namespace DataAccess
 {
     public class ProductDAO
     {
+        public static List<Product> GetProducts()
+        {
+            var listProducts = new List<Product>();
+            try
+            {
+                using (var context = new DBContext())
+                {
+                    listProducts = context.Products.ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return listProducts;
+        }
+
+        public static void Insert(Product product)
+        {
+            try
+            {
+                using (var context = new DBContext())
+                {
+                    var check = context.Products.SingleOrDefault(p => p.ProductName == product.ProductName);
+                    if (check == null)
+                    {
+                        context.Products.Add(product);
+                        context.SaveChanges();
+                    }
+                    else
+                    {
+                        throw new Exception("This product has already existed!");
+                    }
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public static void Update(Product product)
+        {
+            try
+            {
+                using (var context = new DBContext())
+                {
+                    var check = context.Products.SingleOrDefault(p => p.ProductName == product.ProductName);
+                    if (check == null)
+                    {
+                        context.Entry<Product>(product).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                        context.SaveChanges();
+                    }
+                    else
+                    {
+                        throw new Exception("This product has already existed!");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public static void Delete(Product product)
+        {
+            try
+            {
+                using (var context = new DBContext())
+                {
+                    var check = context.Products.SingleOrDefault(c => c.ProductID == product.ProductID);
+                    if(check != null)
+                    {
+                        context.Products.Remove(check);
+                        context.SaveChanges();
+                    }                    
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
     }
 }
