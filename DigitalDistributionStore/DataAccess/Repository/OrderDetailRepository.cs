92638669238b3f625088c12e7a6275cd062d17c2
WilliamTrung <52268132+WilliamTrung@users.noreply.github.com>
2021-10-28 14:19:21 +0700

    Init template

---
+++ DigitalDistributionStore/DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,13 @@
+﻿using DataAccess.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class OrderDetailRepository : IOrderDetail
+    {
+    }
+}
