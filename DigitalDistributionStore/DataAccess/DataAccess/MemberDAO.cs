955ff3223f40663d670dd7bd317aaeb74ac933e4
WilliamTrung <52268132+WilliamTrung@users.noreply.github.com>
2021-10-29 17:35:05 +0700

    Change MemberDAO, OrderDAO, OrderDetailDAO into project

--- DigitalDistributionStore/DataAccess/DataAccess/MemberDAO.cs
+++ DigitalDistributionStore/DataAccess/DataAccess/MemberDAO.cs
@@ -1,8 +1,73 @@
-﻿using System;
+﻿using BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DataAccess
 {
     public class MemberDAO
     {
+        public static List<Member> GetMembers()
+        {
+            var ListMember = new List<Member>();
+            try
+            {
+                using (var context = new DBContext())
+                {
+                    ListMember = context.Members.ToList();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return ListMember;
+        }
+        public static void AddNew(Member member)
+        {
+            try
+            {
+                using (var context = new DBContext())
+                {
+                    context.Add(member);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static void Remove(Member member)
+        {
+            try
+            {
+                using (var context = new DBContext())
+                {
+                    context.Remove(member);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static void Update(Member member)
+        {
+            try
+            {
+                using (var context = new DBContext())
+                {
+                    context.Entry<Member>(member).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
     }
 }
--- DigitalDistributionStore/DataAccess/DataAccess/OrderDetailDAO.cs
+++ DigitalDistributionStore/DataAccess/DataAccess/OrderDetailDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessObject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,67 @@ namespace DataAccess
 {
     public class OrderDetailDAO
     {
+        public static List<OrderDetail> GetOrderDetails()
+        {
+            var ListOrderDetails = new List<OrderDetail>();
+            try
+            {
+                using (var context = new DBContext())
+                {
+                    ListOrderDetails = context.OrderDetails.ToList();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return ListOrderDetails;
+        }
+        public static void AddNew(OrderDetail orderDetail)
+        {
+            try
+            {
+                using (var context = new DBContext())
+                {
+                    context.Add(orderDetail);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static void Remove(OrderDetail orderDetail)
+        {
+            try
+            {
+                using (var context = new DBContext())
+                {
+                    context.Remove(orderDetail);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static void Update(OrderDetail orderDetail)
+        {
+            try
+            {
+                using (var context = new DBContext())
+                {
+                    context.Entry<OrderDetail>(orderDetail).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
     }
 }
